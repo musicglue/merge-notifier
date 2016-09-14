e64f951b12dafdf0c50d27e7a20a5e9b973c99fe
Alex <j@dytry.ch>
2016-09-14 18:25:04 +0100

    extract subject PR number from commit message

--- merge-notifier.js
+++ merge-notifier.js
@@ -2,17 +2,33 @@
 
 const Octokat = require('octokat');
 
+const mergeCommitRegex = /^merge pull request #(\d+)/i;
+
+const isMergeCommit = message =>
+  !!mergeCommitRegex.test(message)
+
+const extractPullRequestNumber = message =>
+  message.match(mergeCommitRegex)[1];
+
 const github = new Octokat({
   username: process.env.GH_USERNAME,
   password: process.env.GH_PASSWORD,
 });
 
-const args = process.argv.slice(2);
-if (args.length !== 3) {
+if (process.argv.length !== 5) {
   console.log('Usage:');
-  console.log('merge-notifier <organisation> <repo> <pr number>');
+  console.log('merge-notifier <organisation> <repo> <pr number or commit message>');
   process.exit(1);
 }
+
+const parseArgs = (organisation, repo, prNumber) => {
+  if (/^\d+$/.test(prNumber)) return [organisation, repo, prNumber];
+  if (isMergeCommit(prNumber)) return [organisation, repo, extractPullRequestNumber(prNumber)];
+  console.log('Not a PR merge commit');
+  process.exit(0);
+};
+
+const args = parseArgs(...process.argv.slice(2));
 
 const fetchAllPaged = pagePromise =>
   pagePromise.then(page =>
@@ -40,14 +56,6 @@ const commentOnPR = (organisation, repo, prNumber, comment) =>
     .comments
     .create({ body: comment });
 
-const mergeCommitRegex = /^merge pull request #(\d+)/i;
-
-const isMergeCommit = ({ commit: { message } }) =>
-  !!mergeCommitRegex.test(message)
-
-const extractPullRequestNumber = ({ commit: { message } }) =>
-  message.match(mergeCommitRegex)[1];
-
 const notificationMessage = pull =>
   `The code in this pull request was just merged into \`${pull.base.ref}\` from ` +
   `\`${pull.head.ref}\` by ${pull.mergedBy.login} as part of [${pull.title}](${pull.html.url}).`;
@@ -59,6 +67,7 @@ Promise
   ])
   .then(([pull, commits]) =>
     Promise.all(commits
+      .map(({ commit }) => commit.message)
       .filter(isMergeCommit)
       .map(extractPullRequestNumber)
       .map(prNumber =>
